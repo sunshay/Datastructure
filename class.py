58753d0d3d15a23304cc4c171fe00f43793a0b4c
sunshay <sunshayina@outlook.com>
2022-05-06 00:30:46 -0700

    commit constructor class

---
+++ class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+class student():
+    def __init__(self,fname,lname,age,section): 
+        self.firstname = fname
+        self.lastname = lname
+        self.age = age
+        self.section = section
+
+subject = student(Mike, Doussey, 35, male)
+print(subject)
