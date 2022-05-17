c9432151b72044c79867a68cef3e7218c4193bb7
sunshay <sunshayina@outlook.com>
2022-05-17 11:25:41 -0700

    create a merge list

---
+++ merge_2_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+A = [1,2,3,4,5]
+B = ["this", "is", "a", "list"]
+
+merges = A + B
+
+## or Makes list1 longer by appending the elements of list2 at the end.
+#list_1.extend(list_2)
+#https://thispointer.com/python-how-to-merge-two-or-more-lists/
+
+print(merges)
