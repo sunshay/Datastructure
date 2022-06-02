f9a4c489d909fe2b8c03e3807518f6bf15b84a5e
sunshay <sunshayina@outlook.com>
2022-06-01 20:59:05 -0700

    manipulate list

---
+++ list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# A=[]
+# for i in range(1,1000):
+#     A.append(i**2)
+
+# print(A)
+
+
+# B=[]
+# for i in range(1,11):
+#     B.append(i*i)
+    
+# print(B)
+
+
+#  add a function
+
+# def square_num(**args):
+#     C = []
+#     for i in **args:
+#         C.append(i*i)
+#     return C
+# mynum = square_num(1,2,3,4,5)
+# print(mynum)
+
+C = [1,2,3,4] #list
+D = ['noir','bleu','rouge','gris']
+E = zip(C,D)    
+print(E)
